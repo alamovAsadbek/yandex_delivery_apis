b2ebab3175d6758b154568f08fe8751a2c91a564
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 22:48:56 +0500

    cahnge

--- app_users/serializers.py
+++ app_users/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import UserModel
 
 
-class RegisterSerializer(serializers.ModelSerializer):
+class UserModelSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
