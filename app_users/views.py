d3f12e5fa04f912b0079e5d8e89be39e86882328
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 22:33:44 +0500

    change

--- app_users/views.py
+++ app_users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,40 +6,14 @@ from .models import UserModel
 from .serializers import RegisterSerializer, LoginSerializer, DeleteUserSerializer
 
 
-class RegisterView(APIView):
+class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     queryset = UserModel.objects.all()
 
     # permission_classes = [IsAdminUser]
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
         serializer.save()
-        response = {
-            'success': True,
-            'message': 'User registered successfully.'
-        }
-        return Response(response, status=status.HTTP_201_CREATED)
-
-    def get(self, request, *args, **kwargs):
-        users = UserModel.objects.all()
-        serializer = self.serializer_class(users, many=True)
-        response = {
-            'success': True,
-            'message': 'List of users',
-            'data': serializer.data
-        }
-        return Response(response, status=status.HTTP_200_OK)
-
-    def delete(self, request, *args, **kwargs):
-        user = UserModel.objects.get(id=kwargs['pk'])
-        user.delete()
-        response = {
-            'success': True,
-            'message': 'User deleted successfully.'
-        }
-        return Response(response, status=status.HTTP_200_OK)
 
 
 class LoginView(APIView):
