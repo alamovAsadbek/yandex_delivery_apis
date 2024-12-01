734d2e21d6dd9b32aa43bacfb863d43fd2197429
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 23:03:00 +0500

    cahnge

--- app_common/pagination.py
+++ app_common/pagination.py
@@ -2,6 +2,9 @@ from rest_framework import pagination
 
 
 class CustomPagination(pagination.PageNumberPagination):
+    """
+    Custom pagination class for pagination.
+    """
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 20
