12acd198d6b999ad8d10ff3082c2bf86554cd868
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 13:23:12 +0500

    change

--- app_users/models.py
+++ app_users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class UserModel(AbstractUser):
-    pass
+    role = models.CharField(default='user', max_length=100)
+    status = models.CharField(default='active', max_length=100)
