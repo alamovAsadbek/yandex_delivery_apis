16b24283af7169fc0984b6e7e1c175a2176fa7d5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 13:31:55 +0500

    change

--- app_users/views.py
+++ app_users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import UserModel
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    queryset = UserModel.objects.all()
