6e0136b1b6a6250b60516fcfdccc6dcca30ff8bf
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 17:31:06 +0500

    change

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,20 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('app_common.urls')),
 ]
 
+# JWT Authentication
 urlpatterns += [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+# Media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
