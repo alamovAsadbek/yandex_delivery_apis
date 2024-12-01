6b356ac2cab05f5c88736b083891cea067087657
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 18:07:44 +0500

    change

--- app_users/serializers.py
+++ app_users/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from .models import UserModel
 
 
@@ -12,17 +11,27 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = UserModel
         fields = ['id', 'first_name', 'last_name', 'phone_number', 'password', 'confirm_password']
         extra_kwargs = {'password': {'write_only': True}}
-
         username_field = 'phone_number'
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
         confirm_password = validated_data.pop('confirm_password', None)
         username = validated_data.pop('phone_number', None)
+
         if not username:
             raise serializers.ValidationError({'phone_number': 'Phone number is required.'})
+
         if password != confirm_password:
             raise serializers.ValidationError({'password': 'Passwords do not match.'})
+
+        # Check if phone number already exists
+        if UserModel.objects.filter(phone_number=username).exists():
+            raise serializers.ValidationError({'phone_number': 'This phone number is already registered.'})
+
+        # Add phone_number as username
+        validated_data['username'] = username
+
+        # Create the user
         user = UserModel.objects.create_user(**validated_data)
         user.set_password(password)
         user.save()
