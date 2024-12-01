5a819ac26ee63587e61a4364cc1e37295e2bfd9a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 17:36:09 +0500

    change

--- app_users/serializers.py
+++ app_users/serializers.py
@@ -5,14 +5,20 @@ from .models import UserModel
 
 class RegisterSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True)
-    phone_number = serializers.CharField(required=True)
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
 
     class Meta:
         model = UserModel
-        fields = ['id', 'first_name', 'last_name', 'password', 'confirm_password']
+        fields = ['id', 'first_name', 'last_name', 'phone_number', 'password', 'confirm_password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        print(validated_data['password'])
+        password = validated_data.pop('password', None)
+        confirm_password = validated_data.pop('confirm_password', None)
+        if password != confirm_password:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        user = UserModel.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
