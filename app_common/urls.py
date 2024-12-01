a861f7cf068c8e5944f8e37c0df05798ef73c659
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 22:38:50 +0500

    change

--- app_common/urls.py
+++ app_common/urls.py
@@ -5,4 +5,5 @@ from app_users import views as user_views
 urlpatterns = [
     path('auth/register/', user_views.RegisterView.as_view(), name='register'),
     path('auth/login/', user_views.LoginView.as_view(), name='login'),
+    path('auth/logout/', user_views.LogoutView.as_view(), name='logout'),
 ]
