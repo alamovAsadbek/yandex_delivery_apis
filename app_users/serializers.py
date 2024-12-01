a07086ee12cff097e7d99490abc64acd4b97bec5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 13:29:56 +0500

    add RegisterSerializers class

---
+++ app_users/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import UserModel
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+    phone_number = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    class Meta:
+        model = UserModel
+        fields = ['id', 'first_name', 'last_name', 'password', 'confirm_password']
+        extra_kwargs = {'password': {'write_only': True}}
