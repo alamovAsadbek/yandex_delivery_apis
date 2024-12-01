1d7d435da0616c6c24dfbb82e5942dfd240a6d63
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 21:59:52 +0500

    change

--- app_users/views.py
+++ app_users/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import UserModel
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 
 
 class RegisterView(generics.CreateAPIView):
@@ -16,5 +17,13 @@ class RegisterView(generics.CreateAPIView):
 
 
 class LoginView(APIView):
-    serializer_class = RegisterSerializer
+    serializer_class = LoginSerializer
     queryset = UserModel.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success': True
+        }
+        return Response(response, status=status.HTTP_200_OK)
