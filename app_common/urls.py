073502ecbb06b2659958e26f7bea47fb3cfd5ded
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-01 22:47:50 +0500

    cahnge

--- app_common/urls.py
+++ app_common/urls.py
@@ -8,8 +8,8 @@ router = DefaultRouter()
 router.register('users', user_views.UserListView, basename='user')
 
 urlpatterns = [
-    # auth urls
-    path('auth/register/', user_views.RegisterView.as_view(), name='register'),
-    path('auth/login/', user_views.LoginView.as_view(), name='login'),
-    path('auth/logout/', user_views.LogoutView.as_view(), name='logout'),
-]
+                  # auth urls
+                  path('auth/register/', user_views.RegisterView.as_view(), name='register'),
+                  path('auth/login/', user_views.LoginView.as_view(), name='login'),
+                  path('auth/logout/', user_views.LogoutView.as_view(), name='logout'),
+              ] + router.urls
