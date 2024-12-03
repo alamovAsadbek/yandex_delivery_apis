f1be7061d9a9a3f5f6bf1e00847c1039665ad13f
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-12-03 22:50:24 +0500

    Add BranchModel with language

--- app_branch/models.py
+++ app_branch/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from app_common.models import BaseModel
+from app_company.models import RestaurantModel
+
+User = get_user_model()
+
+
+class BranchModel(BaseModel):
+    name = models.CharField(max_length=64, verbose_name=_("Name"))
+    address = models.CharField(max_length=255, verbose_name=_("Address"))
+    phone_number = models.CharField(max_length=255, verbose_name=_("Phone Number"))
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    manager = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="branches",
+        verbose_name=_("Manager")
+    )
+    restaurant = models.ForeignKey(
+        RestaurantModel,
+        on_delete=models.CASCADE,
+        related_name="branches",
+        verbose_name=_("Restaurant")
+    )
+    is_active = models.BooleanField(default=True, verbose_name=_("Is Active"))
+
+    class Meta:
+        verbose_name = _("Branch")
+        verbose_name_plural = _("Branches")
+
+    def __str__(self):
+        return self.name
--- app_company/models.py
+++ app_company/models.py
@@ -8,11 +8,16 @@ User = get_user_model()
 
 
 class RestaurantModel(BaseModel):
-    name = models.CharField(max_length=100)
-    manager = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="restaurants")
-    phone_number = models.CharField(max_length=20)
-    logo = models.ImageField(upload_to="restaurant_logos/")
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+    manager = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="restaurants",
+        verbose_name=_('Manager')
+    )
+    phone_number = models.CharField(max_length=20, verbose_name=_('Phone Number'))
+    logo = models.ImageField(upload_to="restaurant_logos/", verbose_name=_('Logo'))
+    is_active = models.BooleanField(default=True, verbose_name=_('Is Active'))
 
     class Meta:
         verbose_name = _("Restaurant")
