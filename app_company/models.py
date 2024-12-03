ae9ece0127559e31586be896e2724b8edb3a0dde
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-12-03 22:44:01 +0500

    Add RestaurantModel with language

--- app_company/models.py
+++ app_company/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from app_common.models import BaseModel
+
+User = get_user_model()
+
+
+class RestaurantModel(BaseModel):
+    name = models.CharField(max_length=100)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="restaurants")
+    phone_number = models.CharField(max_length=20)
+    logo = models.ImageField(upload_to="restaurant_logos/")
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("Restaurant")
+        verbose_name_plural = _("Restaurants")
+
+    def __str__(self):
+        return self.name
