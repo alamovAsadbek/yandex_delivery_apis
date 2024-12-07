b4b00e680709b994c30af29e60b5291d222b12e4
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-12-07 22:46:03 +0500

    add Docstrings to app_products.models

--- app_products/models.py
+++ app_products/models.py
@@ -5,6 +5,12 @@ from app_common.models import BaseModel
 
 
 class CategoryModel(models.Model):
+    """
+    Represents a category of products.
+
+    name: The name of the category.
+    status: Indicates whether the category is active or not.
+    """
     name = models.CharField(max_length=200)
     status = models.BooleanField(default=True)
 
@@ -16,7 +22,16 @@ class CategoryModel(models.Model):
         verbose_name_plural = 'Categories'
 
 
-class ProductsModel(models.Model):
+class ProductsModel(BaseModel):
+    """
+    Represents a product.
+
+    name: The name of the product.
+    description: A brief description of the product.
+    price: The price of the product.
+    category: The category of the product.
+    status: Indicates whether the product is active or not.
+    """
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -27,9 +42,6 @@ class ProductsModel(models.Model):
     )
     status = models.BooleanField(default=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return _(self.name)
 
@@ -39,6 +51,15 @@ class ProductsModel(models.Model):
 
 
 class ProductImageModel(BaseModel):
+    """
+    Represents an image associated with a product.
+
+    product: The product the image belongs to.
+    image: The image file.
+    is_main_image: Indicates whether this image is the main image for the product.
+    status: Indicates whether the image is active or not.
+    """
+    # fields
     product = models.ForeignKey(
         ProductsModel,
         on_delete=models.CASCADE,
