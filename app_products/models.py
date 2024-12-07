eccdfbc63cf9cf0a36c0c869c231776e5d52dcee
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-12-07 22:40:37 +0500

    add category filed to ProductModel

--- app_products/models.py
+++ app_products/models.py
@@ -18,7 +18,9 @@ class CategoryModel(models.Model):
 
 class ProductsModel(models.Model):
     name = models.CharField(max_length=200)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, related_name="products")
     status = models.BooleanField(default=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
