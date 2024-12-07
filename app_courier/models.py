06a18175858a8b83cb1f3c479367b1c8cfe1d9e6
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-07 13:14:42 +0500

    change product model

--- app_courier/models.py
+++ app_courier/models.py
@@ -1,8 +1,7 @@
-from django.db import models
-from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
-
 from app_common.models import BaseModel
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
--- app_products/models.py
+++ app_products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Foods(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return _(self.name)
+
+    class Meta:
+        verbose_name = _('Food')
+        verbose_name_plural = _('Foods')
