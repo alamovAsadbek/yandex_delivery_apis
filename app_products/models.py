903b1ab24963ebf066b3a3818317b6d5b24d83b7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-07 13:15:08 +0500

    change model name

--- app_products/models.py
+++ app_products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Foods(models.Model):
+class Products(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.BooleanField(default=True)
