f08927e32762c3f5a11719457df4539a140ad32d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-12-07 15:16:50 +0500

    change

--- app_products/models.py
+++ app_products/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from app_common import models as common_model
 from app_company import models as company_models
 
 
-class ProductsModel(models.Model):
+class ProductsModel(common_model.BaseModel):
     restaurant = models.ForeignKey(company_models.RestaurantModel, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -22,16 +20,13 @@ class ProductsModel(models.Model):
         verbose_name_plural = _('Foods')
 
 
-class ProductImageModel(models.Model):
+class ProductImageModel(common_model.BaseModel):
     product = models.ForeignKey(ProductsModel, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='products')
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.product.name
 
     class Meta:
-        verbose_name = _('Product Image')
-        verbose_name_plural = _('Product Images')
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'
