26066fc9205f6e234501fb95e1040f1f546f9597
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-12-07 22:41:23 +0500

    add description filed to ProductModel

--- app_products/models.py
+++ app_products/models.py
@@ -20,7 +20,11 @@ class ProductsModel(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, related_name="products")
+    category = models.ForeignKey(
+        CategoryModel,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
     status = models.BooleanField(default=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
