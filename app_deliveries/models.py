f27dc623fa1b3a57c71202e1ffbfa5998a02d4d4
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-12-09 10:08:31 +0500

    Changes and Improvings

--- app_deliveries/models.py
+++ app_deliveries/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from app_branch.models import BranchModel
 from app_common.models import BaseModel
 from app_products.models import ProductsModel
 from app_users.models import UserLocations
@@ -9,6 +10,9 @@ User = get_user_model()
 
 
 class OrderStatus(models.TextChoices):
+    """
+    OrderStatus is a class that contains choices for the order status.
+    """
     PENDING_COURIER = 'pending_for_courier', 'Pending for a Courier'
     PENDING_RESTAURANT = 'pending_for_restaurant', 'Pending for a Restaurant'
     CONFIRMED_COURIER = 'confirmed_by_courier', 'Confirmed by a Courier'
@@ -19,6 +23,13 @@ class OrderStatus(models.TextChoices):
 
 
 class OrderItemModel(models.Model):
+    """
+    OrderItem model represents an item in an order.
+    product: The product associated with the order item.
+    quantity: The quantity of the product ordered.
+    price_per_item: The price per unit of the product.
+    total_price: The total price for the order item.
+    """
     product = models.ForeignKey(
         ProductsModel,
         on_delete=models.CASCADE,
@@ -38,11 +49,33 @@ class OrderItemModel(models.Model):
 
 
 class OrderModel(BaseModel):
+    """
+    Order model represents an order placed by a user in the application.
+    branch: The branch where the order is placed.
+    user: The user who placed the order.
+    courier: The courier assigned to the order.
+    order_status: The status of the order being placed.
+    order_items: The items in the order.
+    delivery_address: The address where the order is to be delivered.
+    """
+    branch = models.ForeignKey(
+        BranchModel,
+        on_delete=models.SET_NULL,
+        related_name='orders',
+        verbose_name='Branch',
+        null=True
+    )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='orders',
         verbose_name='User'
+    )
+    courier = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='orders',
+        verbose_name='Courier'
     )
     order_status = models.CharField(
         max_length=20,
